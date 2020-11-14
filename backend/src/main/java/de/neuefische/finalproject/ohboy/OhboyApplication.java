5673f546bf63cc1cc1abf86b5ce46dd8e3b8a047
Elisabeth Seitz <elisabeth@seitz-jossa.de>
2020-11-14 19:46:41 +0100

    init spring server

---
+++ backend/src/main/java/de/neuefische/finalproject/ohboy/OhboyApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.finalproject.ohboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhboyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhboyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/neuefische/finalproject/ohboy/OhboyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.finalproject.ohboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhboyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
