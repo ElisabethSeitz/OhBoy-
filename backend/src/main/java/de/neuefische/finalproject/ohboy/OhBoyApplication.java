eaf1de94b8c5c4305e339f621ef7e8b3fe466f23
Elisabeth Seitz <elisabeth@seitz-jossa.de>
2020-11-14 20:25:08 +0100

    rename application

--- backend/src/main/java/de/neuefische/finalproject/ohboy/OhBoyApplication.java
+++ backend/src/main/java/de/neuefische/finalproject/ohboy/OhBoyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OhboyApplication {
+public class OhBoyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OhboyApplication.class, args);
+		SpringApplication.run(OhBoyApplication.class, args);
 	}
 
 }
