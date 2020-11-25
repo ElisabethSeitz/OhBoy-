e2bf014bd67f5fb22a5b65b054e5061d3cfff7d1
Elisabeth Seitz <elisabeth@seitz-jossa.de>
2020-11-25 17:19:36 +0100

    add update-task-method to backend

--- backend/src/main/java/de/neuefische/finalproject/ohboy/controller/TaskController.java
+++ backend/src/main/java/de/neuefische/finalproject/ohboy/controller/TaskController.java
@@ -2,11 +2,15 @@ package de.neuefische.finalproject.ohboy.controller;
 
 import de.neuefische.finalproject.ohboy.dto.AddMonsterDto;
 import de.neuefische.finalproject.ohboy.dto.AddTaskDto;
+import de.neuefische.finalproject.ohboy.dto.UpdateMonsterDto;
+import de.neuefische.finalproject.ohboy.dto.UpdateTaskDto;
 import de.neuefische.finalproject.ohboy.model.Monster;
 import de.neuefische.finalproject.ohboy.model.Task;
 import de.neuefische.finalproject.ohboy.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.List;
@@ -30,5 +34,13 @@ public class TaskController {
     @PostMapping("{monsterId}/tasks")
     public Task add(@RequestBody AddTaskDto dto, @PathVariable String monsterId, Principal principal){
         return this.taskService.add(dto, monsterId, principal.getName());
+    }
+
+    @PutMapping("{monsterId}/tasks/{taskId}")
+    public Task update(@RequestBody UpdateTaskDto updatedTask, @PathVariable String taskId, Principal principal) {
+        if(!taskId.equals(updatedTask.getId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        return taskService.update(updatedTask, principal.getName());
     }
 }
