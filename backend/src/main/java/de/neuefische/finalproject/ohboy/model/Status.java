af2b04a43f3768b087502df2d2bf4ecc31c91977
Elisabeth Seitz <elisabeth@seitz-jossa.de>
2020-11-26 16:27:22 +0100

    fix backend tests

--- backend/src/main/java/de/neuefische/finalproject/ohboy/model/Status.java
+++ backend/src/main/java/de/neuefische/finalproject/ohboy/model/Status.java
@@ -1,11 +1,6 @@
 package de.neuefische.finalproject.ohboy.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum Status {
-    @JsonProperty("OPEN")
     OPEN,
-    @JsonProperty("DONE")
     DONE
 }
