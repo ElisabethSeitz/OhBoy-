be216f44e83ef0a4b6f46cedbd5f3f8e3289f8c4
Elisabeth Seitz <elisabeth@seitz-jossa.de>
2020-11-30 14:44:01 +0100

    styling first steps

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,24 +2,37 @@ import React from 'react';
 import MonsterContext from '../contexts/MonsterContext';
 import styled from 'styled-components/macro';
 import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
-export default function Header({ currentMonsterId }) {
+export default function Header({ currentMonsterId, taskId }) {
   const { monsters } = useContext(MonsterContext);
+  const history = useHistory();
 
   const monstersToDisplay = monsters?.filter(
     (monster) => monster.id !== currentMonsterId
   );
 
+  const monsterLink = (taskId, monsterId) => {
+    if (taskId) {
+      return history.push('/monsters/' + monsterId + '/tasks');
+    }
+    return history.push('/monsters/' + monsterId + '/rewards');
+  };
+
   return (
     <HeaderStyled>
       <HeadingStyled>OhBoy!</HeadingStyled>
-      <ImageContainer>
+      <div>
         {monstersToDisplay.map((monster) => (
           <div key={monster.id}>
-            <HeaderMonsterImage src={monster.image} alt={monster.name} />
+            <HeaderMonsterImage
+              src={monster.image}
+              alt={monster.name}
+              onClick={monsterLink(taskId, monster.id)}
+            />
           </div>
         ))}
-      </ImageContainer>
+      </div>
     </HeaderStyled>
   );
 }
@@ -27,7 +40,6 @@ export default function Header({ currentMonsterId }) {
 const HeaderStyled = styled.header`
   display: grid;
   grid-template-columns: 25% 50% 25%;
-  grid-template-rows: 1fr;
   padding: var(--size-s) 0;
   margin: 0 var(--size-s);
   border-bottom: var(--black-border);
@@ -44,8 +56,4 @@ const HeaderMonsterImage = styled.img`
   width: 25px;
   height: 25px;
   float: left;
-`;
-
-const ImageContainer = styled.div`
-  align-self: end;
 `;
