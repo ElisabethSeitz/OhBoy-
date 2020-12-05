037af284c128ed3ecc9a38fdb8447c5fe2aeb500
Elisabeth Seitz <elisabeth@seitz-jossa.de>
2020-12-05 19:32:50 +0100

    refactor Confirmation

--- frontend/src/components/Confirmation.js
+++ frontend/src/components/Confirmation.js
@@ -1,50 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Confirmation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      label: props.label,
-      question: props.question,
-      confirmed: false,
-    };
-  }
-  render() {
-    return (
-      <div>
-        {(this.state.confirmed !== true && (
-          <button
-            type="button"
-            onClick={() => {
-              this.setState({ confirmed: true });
-            }}
-          >
-            {this.state.label}
+export default function Confirmation({ question, label, onClick }) {
+  const [confirmed, setConfirmed] = useState(false);
+
+  return (
+    <>
+      <DisplayButtonOrConfirmation />
+    </>
+  );
+
+  function DisplayButtonOrConfirmation() {
+    if (confirmed === false) {
+      return (
+        <button type="button" onClick={() => setConfirmed(true)}>
+          {label}
+        </button>
+      );
+    }
+    if (confirmed === true) {
+      return (
+        <div>
+          <p>{question}</p>
+          <button type="button" onClick={(event) => onClick(event)}>
+            Yes
           </button>
-        )) || (
-          <div>
-            <span>{this.state.question}</span>
-            <button
-              type="button"
-              onClick={(event) => {
-                this.props.onClick(event);
-              }}
-            >
-              Yes
-            </button>
-            <button
-              type="button"
-              onClick={() => {
-                this.setState({ confirmed: false });
-              }}
-            >
-              No
-            </button>
-          </div>
-        )}
-      </div>
-    );
+          <button type="button" onClick={() => setConfirmed(false)}>
+            No
+          </button>
+        </div>
+      );
+    }
   }
 }
-
-export default Confirmation;
