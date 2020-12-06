6b591b7b0199a9fde689ef9e833d22011a928379
Elisabeth Seitz <elisabeth@seitz-jossa.de>
2020-12-06 17:31:26 +0100

    add MonsterPageLayout

--- frontend/src/pages/MonsterPage.js
+++ frontend/src/pages/MonsterPage.js
@@ -2,14 +2,20 @@ import React from 'react';
 import MonsterList from '../lists/MonsterList';
 import Header from '../components/Header';
 import AddButton from '../components/AddButton';
+import styled from 'styled-components/macro';
 
 export default function MonsterPage() {
   return (
-    <>
+    <MonsterPageLayout>
       <Header icons={false} monster={true} />
-      <div> </div>
       <MonsterList />
       <AddButton monster={true} />
-    </>
+    </MonsterPageLayout>
   );
 }
+
+const MonsterPageLayout = styled.div`
+  display: grid;
+  grid-template-rows: min-content 1fr;
+  height: 100vh;
+`;
